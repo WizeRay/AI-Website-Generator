7cd8f33d16fa2a8097e0c13dc5f8d4f25beb75f9
WizeRay <shreyasacharya503@gmail.com>
2026-06-11 15:28:36 -0700

    MyProject page complete,footer complete

---
+++ Client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+function Footer() {
+  return (
+    <div className="text-center py-4 text-gray-400 text-sm border-t border-gray-800 mt-24">
+      <p>Copyright &copy; 2026 AI Website Builder - WizeRay</p>
+    </div>
+  )
+}
+
+export default Footer
--- Client/src/layouts/RootLayout.jsx
+++ Client/src/layouts/RootLayout.jsx
@@ -1,11 +1,12 @@
 import Navbar from "../components/Navbar";
 import { Outlet } from "react-router";
-
+import Footer from "../components/Footer";
 function RootLayout() {
   return (
     <div className="bg-black min-h-screen">
         <Navbar/>
         <Outlet/>
+        <Footer/>
     </div>
   )
 }
